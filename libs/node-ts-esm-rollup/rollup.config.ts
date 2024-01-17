77762ea9695ca90affff45e46a26617ebfe50f21
Samantha Nguyen <contact@samanthanguyen.me>
2024-01-16 20:37:52 -0600

    Update rollup.config.ts

--- libs/node-ts-esm-rollup/rollup.config.ts
+++ libs/node-ts-esm-rollup/rollup.config.ts
@@ -1,6 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
-import { dts } from "rollup-plugin-dts";
+import { dts } from 'rollup-plugin-dts';
 
 export default defineConfig([
 	{
