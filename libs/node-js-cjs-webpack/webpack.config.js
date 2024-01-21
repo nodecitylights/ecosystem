65eef1ddb7367e1ef922d9f594f5b6f06cb04fcd
Samantha <contact@samanthanguyen.me>
2024-01-21 16:28:17 -0600

    feat: add library template `/libs/node-js-cjs-webpack` (#9)

---
+++ libs/node-js-cjs-webpack/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		library: {
+			name: 'greeting',
+			type: 'commonjs'
+		}
+	},
+};
