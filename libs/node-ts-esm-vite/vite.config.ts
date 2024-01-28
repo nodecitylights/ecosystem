a8b80d7a50be5ab49d9e8a6d14bafd06ae226d8f
Samantha Nguyen <contact@samanthanguyen.me>
2024-01-28 15:35:03 -0600

    chore(vite): use modern JS for path resolving

--- libs/node-ts-esm-vite/vite.config.ts
+++ libs/node-ts-esm-vite/vite.config.ts
@@ -1,12 +1,10 @@
-import { resolve } from 'node:path';
-
 import { defineConfig }	from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
 	build: {
 		lib: {
-			entry: resolve(__dirname, 'src/index.ts'),
+			entry: new URL('src/index.ts', import.meta.url).pathname,
 			formats: ['es'],
 			fileName: () => 'index.mjs',
 		},
