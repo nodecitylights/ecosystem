54ad1e0a22cc70e8e69ad97942f8e7a8dc0c91f5
Samantha <contact@samanthanguyen.me>
2024-02-01 00:08:36 -0600

    chore(ts): add type hinting to webpack config files (#31)

--- libs/node-ts-esm-webpack/webpack.config.cjs
+++ libs/node-ts-esm-webpack/webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require('path');
 const isProduction = process.env.NODE_ENV == 'production';
 
+/** @type {import('webpack').Configuration} */
 const config = {
 	experiments: {
 		outputModule: true,
