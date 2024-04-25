53571e66c82b9ee147461a5c865b8a30574844f3
Samantha <contact@samanthanguyen.me>
2024-04-24 23:52:40 -0500

    feat: add library template `/libs/node-js-esm-webpack` (#42)

---
+++ libs/node-js-esm-webpack/webpack.config.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+const isProduction = process.env.NODE_ENV == 'production';
+
+/** @type {import('webpack').Configuration} */
+const config = {
+	experiments: {
+		outputModule: true,
+	},
+	devtool: 'inline-source-map',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		clean: true,
+		library: {
+			type: 'module'
+		}
+	},
+};
+
+module.exports = () => {
+	config.mode = isProduction ? 'production' : 'development';
+	return config;
+};
