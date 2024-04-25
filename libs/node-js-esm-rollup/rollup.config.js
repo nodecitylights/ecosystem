7487fd2dcc5efce1cb6a6bfedce42ab7565666d1
Samantha Nguyen <contact@samanthanguyen.me>
2024-04-24 23:14:28 -0500

    feat: add library template `/libs/node-js-esm-rollup`

---
+++ libs/node-js-esm-rollup/rollup.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'rollup';
+import { dts } from 'rollup-plugin-dts';
+
+export default defineConfig([
+	{
+		input: 'src/index.js',
+		output: [{ file: 'dist/index.js', format: 'esm' }],
+	},
+	{
+		input: 'src/index.js',
+		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+		plugins: [dts()],
+	}
+])
