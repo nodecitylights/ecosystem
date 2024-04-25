451717c71e714cbaab255f1620ec24cfe15b2cab
Samantha Nguyen <contact@samanthanguyen.me>
2024-04-24 21:43:36 -0500

    feat: add `/libs/node-js-esm-vite`

---
+++ libs/node-js-esm-vite/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig }	from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: new URL('src/index.js', import.meta.url).pathname,
+			formats: ['es'],
+			fileName: () => 'index.mjs',
+		},
+	},
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+})
